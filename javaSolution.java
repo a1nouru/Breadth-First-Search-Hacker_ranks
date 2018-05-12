608939838f06039587cba95df00ae120d42a3e5b
Nouru Muneza <nizzogentl@gmail.com>
2018-05-12 14:12:50 -0700

    Update javaSolution.java

--- javaSolution.java
+++ javaSolution.java
@@ -1,1 +1,16 @@
 
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static class Position{
+        int inde;
+        
+        int stepsMoved;
+        public Position(int i, int s)
+{
+            inde =i;
+            
+            stepsMoved =s;
+    }
+
